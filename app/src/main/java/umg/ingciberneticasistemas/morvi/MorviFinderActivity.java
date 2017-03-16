356d9c35ba7902d219d45b30d4b2842bd27f3f55
luixlive <luixlive@gmail.com>
2017-03-16 12:00:03 -0500

    Vista del recyclerview lista Se agregan barras de progreso Se agregan permisos de bluetooth Se solicita a usuario encender bt Se buscan dispositivos con bloetooth le (No encuentra nada...) -luixlive

--- app/src/main/java/umg/ingciberneticasistemas/morvi/MorviFinderActivity.java
+++ app/src/main/java/umg/ingciberneticasistemas/morvi/MorviFinderActivity.java
@@ -1,15 +1,65 @@
 package umg.ingciberneticasistemas.morvi;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ProgressBar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import umg.ingciberneticasistemas.morvi.adapter.DeviceAdapter;
 
 public class MorviFinderActivity extends AppCompatActivity {
+
+    public final static int REQUEST_ENABLE_BT = 100;
+    private static final long SCAN_PERIOD = 20000;
+
+    private BluetoothAdapter bt_adapter;
+    private BluetoothLeScanner ble_scanner;
+    private Handler search_handler;
+    private DeviceAdapter device_adapter;
+    private boolean scanning = false;
+
+    private ProgressBar progress_bt_find;
+
+    private ScanCallback ble_callback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            super.onScanResult(callbackType, result);
+            BluetoothDevice device = result.getDevice();
+            Log.i("Bluetooth", "Found: " + device.getName());
+            device_adapter.addDevice(device);
+        }
+
+        @Override
+        public void onBatchScanResults(List<ScanResult> results) {
+            super.onBatchScanResults(results);
+            Log.i("Bluetooth", "Found: " + results.size() + " results");
+        }
+
+        @Override
+        public void onScanFailed(int errorCode) {
+            super.onScanFailed(errorCode);
+            Log.i("Bluetooth", "Failed");
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +67,28 @@ public class MorviFinderActivity extends AppCompatActivity {
         setContentView(R.layout.activity_morvi_finder);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        progress_bt_find = (ProgressBar)findViewById(R.id.progress_bt_find);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_renew);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                searchDevices();
+                Snackbar.make(view, "Searching devices", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+
+        BluetoothManager bt_manager = (BluetoothManager)getSystemService(Context.BLUETOOTH_SERVICE);
+        bt_adapter = bt_manager.getAdapter();
+        ble_scanner = bt_adapter.getBluetoothLeScanner();
+        search_handler = new Handler();
+
+        device_adapter = new DeviceAdapter(new ArrayList<BluetoothDevice>());
+        RecyclerView list_devices = (RecyclerView) findViewById(R.id.recycler_view_devices);
+        list_devices.setAdapter(device_adapter);
+
+        searchDevices();
     }
 
     @Override
@@ -47,5 +110,39 @@ public class MorviFinderActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (resultCode == RESULT_OK) {
+                searchDevices();
+            }
+        }
+    }
+
+    public void searchDevices(){
+        if (bt_adapter == null || !bt_adapter.isEnabled()) {
+            Intent enable_bt = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enable_bt, REQUEST_ENABLE_BT);
+            return;
+        }
+
+        if (!scanning) {
+            search_handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    progress_bt_find.setVisibility(View.INVISIBLE);
+                    ble_scanner.startScan(ble_callback);
+                    scanning = false;
+                    Log.i("Bluetooth", "Scan finished");
+                }
+            }, SCAN_PERIOD);
+
+            progress_bt_find.setVisibility(View.VISIBLE);
+            ble_scanner.stopScan(ble_callback);
+            scanning = true;
+            Log.i("Bluetooth", "Scan started");
+        }
     }
 }
--- app/src/main/java/umg/ingciberneticasistemas/morvi/adapter/DeviceAdapter.java
+++ app/src/main/java/umg/ingciberneticasistemas/morvi/adapter/DeviceAdapter.java
@@ -1,8 +1,17 @@
 package umg.ingciberneticasistemas.morvi.adapter;
 
+import android.bluetooth.BluetoothDevice;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import umg.ingciberneticasistemas.morvi.R;
 
 /**
  * Created by luchavez on 16/03/2017.
@@ -10,25 +19,76 @@ import android.view.ViewGroup;
 
 public class DeviceAdapter extends RecyclerView.Adapter<DeviceAdapter.DeviceHolder> {
 
+    private ArrayList<BluetoothDevice> devices;
+
+    public DeviceAdapter(ArrayList<BluetoothDevice> devices){
+        this.devices = devices;
+    }
+
     @Override
-    public DeviceHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public DeviceAdapter.DeviceHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View layout_device = LayoutInflater.from(
+                parent.getContext()).inflate(R.layout.device_view, parent, false);
+        return new DeviceHolder(layout_device);
     }
 
     @Override
     public void onBindViewHolder(DeviceHolder holder, int position) {
-
+        holder.setDeviceName(devices.get(position).getName());
+        holder.setBTIcon(false);
+        holder.showPB(true);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return devices.size();
     }
 
-    public static class DeviceHolder extends RecyclerView.ViewHolder{
+    public void addDevice(BluetoothDevice device){
+        devices.add(device);
+        notifyItemInserted(devices.size() - 1);
+    }
+
+    public static class DeviceHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener{
+
+        private TextView device_name;
+        private ImageView bt_icon;
+        private ProgressBar pb;
 
         public DeviceHolder(View itemView) {
             super(itemView);
+
+            device_name = (TextView)itemView.findViewById(R.id.device_name);
+            bt_icon = (ImageView)itemView.findViewById(R.id.ic_bluetooth_status);
+            pb = (ProgressBar)itemView.findViewById(R.id.progress_loader);
+
+            itemView.setOnClickListener(this);
+        }
+
+        public void setDeviceName(String name){
+            device_name.setText(name);
+        }
+
+        public void setBTIcon(boolean connected){
+            if(connected){
+                bt_icon.setImageResource(R.drawable.ic_bluetooth_connected);
+            } else{
+                bt_icon.setImageResource(R.drawable.ic_bluetooth);
+            }
+        }
+
+        public void showPB(boolean show){
+            if (show){
+                pb.setVisibility(View.VISIBLE);
+            } else {
+                pb.setVisibility(View.INVISIBLE);
+            }
+        }
+
+        @Override
+        public void onClick(View v) {
+            //CONECTAR A DISPOSITIVO
         }
     }
 
